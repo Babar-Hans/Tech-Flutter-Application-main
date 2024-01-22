60cc29eafc0582ab97818e7e2b045ee167b36c42
Babar-Hans <babarhans34@gmail.com>
2024-01-22 13:20:57 +0500

    learn run time constant using final keywork

--- gfSeaching.dart
+++ gfSeaching.dart
@@ -1,45 +1,18 @@
-// void main(){
-// dynamic acheDost = goodFriend(["Babar","Bilal","Sahil","John"]);
-// dynamic saradost = ["Babar","Bilal","Sahil","John"];
-// for(String dost in saradost){
-
-// }
-// print(acheDost);
-// }
-
-// List<String> goodFriend(List<String> allFriend){
-// List<String> finalGoodFriend = [];
-// for(String friend in allFriend){
-// if(friend == "Babar" && friend == "Sahil"){
-//   finalGoodFriend.add(friend);
-// }
-// }
-// return print(finalGoodFriend);
-// }
-
-
-void main() {
-  dynamic acheDost = goodFriend(["Babar", "Bilal", "Sahil", "John"]);
-  List<String> saradost = ["Babar", "Bilal", "Sahil", "John"];
-  
-  for (String dost    in acheDost) {
-    // Do something with dost, or you can remove this loop if not needed
-    print(dost);
-  }
-
-  print(acheDost);
+void main(){
+List<String> acheDost = goodFriend(["Babar","Bilal","Sahil","John"]);
+dynamic saradost = ["Babar","Bilal","Sahil","John"];
+for(String dost in saradost){
+print(dost);
+}
+print(acheDost);
 }
 
-List<String> goodFriend(List<String> allFriend) {
-  List<String> finalGoodFriend = [];
-
-  for (String friend in allFriend) {
-    // Corrected the logical condition from && to ||
-    if (friend == "Babar" || friend == "Sahil") {
-      finalGoodFriend.add(friend);
-    }
-  }
-
-  // Removed unnecessary print statement
-  return finalGoodFriend;
+List<String> goodFriend(List<String> allFriend){
+List<String> finalGoodFriend = [];
+for(String friend in allFriend){
+if(friend == "Babar" || friend == "Sahil"){
+  finalGoodFriend.add(friend);
+}
+}
+return finalGoodFriend;
 }
---
+++ runTimeConst.dart
@@ -0,0 +1,9 @@
+void main(){
+  int a = 20;
+  final b = a +1;
+  print(a);
+  print(b);
+  a = 30;
+  print(a);
+  print(b);
+}
