cbca69e6e2cffef2813a945f03485ebbd7332a9a
Babar-Hans <babarhans34@gmail.com>
2024-01-22 14:53:38 +0500

    foreach is a function who gets only function in arguement

--- listFun.dart
+++ listFun.dart
@@ -1,6 +1,12 @@
 void main(){
   List<String> foods = ["Burger","Zinger","Pizza"];
-  for(String food in foods){
-    print(food);
-  }
+  // for(String food in foods){
+  //   print(food);
+  // }
+  foods.add("Donut");
+  foods.forEach(forEveryFood);
+}
+
+forEveryFood(String foods){
+  print("It is Really Tasty $foods");
 }
