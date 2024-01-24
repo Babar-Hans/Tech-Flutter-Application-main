eeec299f9d30f78c32d4ae4c659629df890ef2d3
Babar-Hans <babarhans34@gmail.com>
2024-01-24 16:17:53 +0500

    annofunction learning

--- annoFun.dart
+++ annoFun.dart
@@ -2,7 +2,14 @@ void main(){
   //type1
   var sum = (int a, int b){
     print(a+b);
-  }
+  };
+sum(10,10);
+
+List<String> dostlog = ["babar","bilal"];
+
+dostlog.forEach((name){
+print(name);
+});
+
 }
 
-sum(10,20);
