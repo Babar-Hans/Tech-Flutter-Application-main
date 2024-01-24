4335f9654a655b4994bceb1844f48093bc96acc2
Babar-Hans <babarhans34@gmail.com>
2024-01-24 14:38:08 +0500

    use map function

--- mapIterative.dart
+++ mapIterative.dart
@@ -1,3 +1,21 @@
 void main(){
-  
+  List<String> foods = ["Burger","Zinger","Pizza"];
+  // for(String food in foods){
+  //   print(food);
+  // }
+  foods.add("Donut");
+  foods.forEach(forEveryFood);
+
+  List<String> firstCharOfEveryFood = foods.map(changeFood).toList();
+  print(firstCharOfEveryFood);
+
+}
+
+String changeFood(String foods){
+  return foods[0];
+}
+
+forEveryFood(String foods){
+  print("It is Really Tasty $foods");
+   
 }
